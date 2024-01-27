838228f68e16e7349f87131b77146e1f166f64e5
justSass <132598247+justSass@users.noreply.github.com>
2024-01-27 13:19:16 -0500

    SoundManager

---
+++ GlobalJam/Assets/YorgelisTest/SoundManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Unity.VisualScripting;
+
+public class SoundManager : MonoBehaviour
+{
+
+    private static SoundManager _instance;
+
+    public static SoundManager myInstance { get { return _instance; } }
+
+    public List<Sound> sounds, music;
+    public AudioSource clipSfx, sfxSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void playSfx(string _sfxSound)
+    {
+        Sound sound =  sounds.Find( x => x.nameClip == _sfxSound);
+
+        if(sound != null) { clipSfx.clip = sound.clip; clipSfx.PlayOneShot(sound.clip); } else { Debug.Log("Not found"); }
+    }
+
+    public void musicSfx(string _sfxMusic)
+    {
+        Sound sound = sounds.Find(x => x.nameClip == _sfxMusic);
+
+        if (sound != null) { clipSfx.clip = sound.clip; clipSfx.Play(); clipSfx.volume = sound.volume; } else { Debug.Log("Not found"); }
+    }
+}
