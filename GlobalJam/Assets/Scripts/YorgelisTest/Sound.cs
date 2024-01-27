5ad64f51eec907a226ef123df58dbf5d21bcf41f
justSass <132598247+justSass@users.noreply.github.com>
2024-01-27 13:15:11 -0500

    Sound

--- GlobalJam/Assets/Scripts/YorgelisTest/Sound.cs
+++ GlobalJam/Assets/Scripts/YorgelisTest/Sound.cs
@@ -1,18 +1,15 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class Sound : MonoBehaviour
+
+[System.Serializable]
+public class Sound
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string nameClip;
+    public AudioClip clip;
+    [Range(0f, 1f)]
+    public float volume;
+
 }
